3076d15f86429305b87bb8ebd7e89343aac40ba6
Fraser Tweedale <ftweedal@redhat.com>
2022-03-04 17:00:00 +1000

    add tmpfiles.py

---
+++ init/tmpfiles.py
@@ -0,0 +1,651 @@
+#!/bin/python3
+
+"""
+
+Script that processes systemd-tmpfiles config files, approximately
+in the manner described by `tmpfiles.d(5)`.
+
+Only creates files/dirs/links, chowns, chmods, sets attributes and
+ACLs.  Does not implement cleanup and removal.
+
+`!` (indicating post-boot unsafety) is ignored, because this script
+is only intended to be used at container startup.
+
+ONLY files in `/usr/lib/tmpfiles.d` are read - not the other
+directories.  Does not perform any conflict resolution (where the
+same target file is mentioned by multiple config files).
+
+"""
+
+import collections
+import glob
+import grp
+import itertools
+import operator
+import os
+import pwd
+import shutil
+import stat
+import subprocess
+import sys
+
+TMPFILES_DIR = "/usr/lib/tmpfiles.d"
+FACTORY_DIR = "/usr/share/factory"
+
+
+def list_tmpfiles_configs():
+    """
+    Return sorted list of absolute paths to tmpfiles configs.
+    """
+    return sorted(
+        os.path.join(TMPFILES_DIR, f)
+        for f in os.listdir(TMPFILES_DIR)
+        if f.endswith(".conf")
+    )
+
+
+def read_tmpfiles_config(path):
+    """
+    Read the tmpfiles config.  Return a `list` of groups of
+    `(path,list_of_actions)` tuples, with paths in lexicographic
+    order.  Therefore, prefix/parent paths are always listed before
+    suffix/child paths.
+
+    """
+    with open(path) as f:
+        lines = f.readlines()
+    actions = (
+        parse_action(line.strip())
+        for line in lines
+        if len(line.strip()) > 0 and not line.startswith("#")
+    )
+
+    # putting things in order.
+    #
+    # pass 1: sort all actions by path.
+    #         output is iterable of (path, action)
+    #
+    actions = sorted(actions, key=operator.itemgetter(0))
+
+    # pass 2: group actions by path.
+    #         output is iterable of (path, [(path, action)])
+    actions = itertools.groupby(actions, key=operator.itemgetter(0))
+
+    # pass 2.1: discard redundant path, dissolve nested tuple.
+    #           output is iterable of (path, [action])
+    actions = ((k, map(operator.itemgetter(1), v)) for k, v in actions)
+
+    # pass 3: sort actions for each path
+    ACTION_ORDER = list(ACTION_MAP.values())
+
+    def f(action):
+        return ACTION_ORDER.index(type(action))
+
+    actions = ((path, sorted(l, key=f)) for path, l in actions)
+
+    # finally, return a list (makes debugging easier)
+    return list(actions)
+
+
+def parse_action(line):
+    """
+    Parse a line, returning a `(path,action)` pair or raising
+    ValueError on parse failure.  This function should NOT be
+    applied to empty lines or comments.
+    """
+    fields = line.split(maxsplit=6)
+    fields += [None] * (7 - len(fields))  # extend to required length
+    typ, path, mode, user, group, age, arg = fields
+
+    boot_only = "!" in typ
+    ignore_error = "-" in typ  # TODO implement
+    remove_mismatched = "=" in typ  # TODO implement
+    typ = typ.strip("!-=")
+
+    action = ACTION_MAP[typ](boot_only, mode, user, group, age, arg)
+    return (path, action)
+
+
+def main(dry_run):
+    config_files = list_tmpfiles_configs()
+    for config_file in config_files:
+        for path, actions in read_tmpfiles_config(config_file):
+            print(f">>> {path}")
+            for action in actions:
+                if dry_run:
+                    print(action)
+                else:
+                    action.apply(path)
+
+
+def _parse_mode(s):
+    if s is None:
+        return None
+
+    mask = False
+    if s.startswith("~"):
+        mask = True
+        s = s[1:]
+
+    return (mask, int(s, base=8))
+
+
+def _parse_user(s):
+    if s is None:
+        return None
+    try:
+        return int(s)
+    except ValueError:
+        return pwd.getpwnam(s)[2]
+
+
+def _parse_group(s):
+    if s is None:
+        return None
+    try:
+        return int(s)
+    except ValueError:
+        return grp.getgrnam(s)[2]
+
+
+def _parse_attrs(s):
+    if not s:  # None or empty string
+        raise ValueError("file attributes cannot be empty")
+    if s[0] not in "+-=":
+        s = "+" + s  # default operation is '+'
+
+    # TODO more complete checks?  For now just take it as-is.
+    return s
+
+
+def _parse_acls(s):
+    if not s:  # None or empty string
+        raise ValueError("file ACLs cannot be empty")
+
+    # TODO more checks?
+    return s
+
+
+def _parse_major_minor(s):
+    if s is None:
+        raise ValueError("major:minor cannot be empty")
+
+    parts = s.split(":")
+    if len(parts) != 2:
+        raise ValueError("major:minor must have 2 parts")
+
+    try:
+        major = int(parts[0])
+        minor = int(parts[1])
+    except ValueError:
+        raise ValueError("major:minor must be integers")
+
+    return (major, minor)
+
+
+def _run_process(desc, args):
+    try:
+        subprocess.check_call(args)
+    except FileNotFoundError:
+        if len(args) < 1:
+            prog = "<undefined>"
+        else:
+            prog = args[0]
+        print(f"failed to {desc}: {prog!r} program not found")
+    except subprocess.CalledProcessError as e:
+        print(f"failed to {desc}: {e}")
+
+
+class Action:
+    # function to parse/massage argument value
+    # If should raise ValueError for invalid input.
+    arg_function = staticmethod(lambda x: x)
+
+    def __init__(self, bootonly, mode, user, group, age, arg):
+        self.bootonly = bootonly
+        self._set("mode", mode, function=_parse_mode)
+        self._set("user", user, function=_parse_user)
+        self._set("group", group, function=_parse_group)
+        self._set("age", age, function=lambda TODO: TODO)
+        self._set("arg", arg, function=self.arg_function)
+
+    def _set(self, k, v, function=lambda x: x):
+        if v == "-":
+            v = None
+        setattr(self, k, function(v))
+
+    def describe(self, path):
+        """Describe the action."""
+        raise NotImplementedError
+
+    def apply(self, path):
+        """Apply the action."""
+        self.apply_one(path)
+
+    def apply_one(self, path):
+        """
+        Apply the action on a single path.
+
+        This function may be called an arbitrary number of times
+        (including zero) for glob-based actions.
+
+        """
+        raise NotImplementedError
+
+    def _chmod(self, path):
+        """Set the file mode.  Caller must ensure file exists."""
+        if self.mode is None:
+            mask = False
+            if os.path.islink(path) or not os.path.isdir(path):
+                mode = 0o644  # is a file or symbolic link
+            else:
+                mode = 0o755  # is a dir
+        else:
+            mask, mode = self.mode
+
+        if mask:
+            r = os.lstat(path).st_mode  # stat file
+
+            # existing mode masks new mode
+            mode &= stat.S_IMODE(r)
+
+            # unset sticky/SUID/SGID unless directory
+            if not stat.S_ISDIR(r):
+                mode &= ~(stat.S_ISUID | stat.S_ISGID | stat.S_ISVTX)
+
+        try:
+            os.chmod(path, mode, follow_symlinks=False)
+        except:
+            print(f"failed to chmod {path!r}")
+
+    def _chown(self, path):
+        """Set the file ownership.  Caller must ensure file exists."""
+        user = self.user
+        if user is None:
+            user = -1
+        group = self.group
+        if group is None:
+            group = -1
+        if user >= 0 or group >= 0:
+            try:
+                os.chown(path, user, group, follow_symlinks=False)
+            except:
+                print(f"failed to chmod {path!r}")
+
+
+class GlobAction(Action):
+    """Action that takes a glob rather than a path."""
+
+    def apply(self, pattern):
+        for path in glob.glob(pattern):
+            self.apply_one(path)
+
+
+class FileCreate(Action):
+    """f - create file with optional content"""
+
+    def apply_one(self, path):
+        if not os.path.lexists(path):
+            with open(path, "w") as f:
+                f.write(self.arg if self.arg is not None else "")
+        self._chmod(path)
+        self._chown(path)
+
+
+class FileCreateOrTruncate(Action):
+    """f+ - create or truncate file, with optional content"""
+
+    def apply_one(self, path):
+        with open(path, "w") as f:
+            f.write(self.arg if self.arg is not None else "")
+        self._chmod(path)
+        self._chown(path)
+
+
+class FileWrite(GlobAction):
+    """w - write to file"""
+
+    # TODO interpret C-style blackslashes in argument.  Also for
+    # other actions (f, f+, w+, ...)
+    def apply_one(self, path):
+        with open(path, "w") as f:
+            f.write(self.arg if self.arg is not None else "")
+        self._chmod(path)
+        self._chown(path)
+
+
+class FileAppend(GlobAction):
+    """w+ - append to file"""
+
+    def apply_one(self, path):
+        with open(path, "a") as f:
+            f.write(self.arg if self.arg is not None else "")
+        self._chmod(path)
+        self._chown(path)
+
+
+class DirCreateAndCleanup(Action):
+    """d - create and cleanup directory"""
+
+    def apply_one(self, path):
+        if not os.path.lexists(path):
+            os.makedirs(path)
+            self._chmod(path)
+            self._chown(path)
+
+
+class DirCreateAndRemove(DirCreateAndCleanup):
+    """D - create and remove directory"""
+
+    # No additional behaviour, until --remove gets implemented
+
+
+class DirCleanup(GlobAction):
+    """e - create and remove directory"""
+
+    def apply_one(self, path):
+        if os.path.isdir(path):
+            self._chmod(path)
+            self._chown(path)
+
+
+class SubvolumeCreate_v(DirCreateAndCleanup):
+    """v - create subvolume or directory"""
+
+    # ignore subvolume behaviour
+
+
+class SubvolumeCreate_q(DirCreateAndCleanup):
+    """q - create subvolume or directory"""
+
+    # ignore subvolume behaviour
+
+
+class SubvolumeCreate_Q(DirCreateAndCleanup):
+    """Q - create subvolume or directory"""
+
+    # ignore subvolume behaviour
+
+
+# TODO p, p+
+
+
+class SymlinkCreate(Action):
+    """L - create symlink"""
+
+    def apply_one(self, path):
+        if not os.path.exists(path):
+            if self.arg is None:
+                # TODO link to /usr/share/factory/FILE
+                # (see tmpfiles.d(5) for details)
+                raise RuntimeError("symlink target not specified")
+            os.symlink(self.arg, path)
+
+
+class SymlinkRecreate(SymlinkCreate):
+    """L+ - [re]create symlink"""
+
+    def apply_one(self, path):
+        if os.path.exists(path):
+            if not os.path.islink(path) and os.path.isdir(path):
+                shutil.rmtree(path)
+            else:
+                os.unlink(path)
+        super().apply(path)
+
+
+class CreateCharDev(Action):
+    """c - create character device node"""
+
+    arg_function = staticmethod(_parse_major_minor)
+
+    def apply_one(self, path):
+        major, minor = self.args
+        if not os.path.lexists():
+            _run_process(
+                f"create character device at {path}",
+                ["mknod", path, c, major, minor],
+            )
+
+
+# TODO c+ b b+ (char and block devices)
+
+
+class Copy(Action):
+    """C - copy file"""
+
+    def apply_one(self, path):
+        src = self.arg
+        if src is None:
+            src = os.path.join(FACTORY_DIR, os.path.relpath(path, start="/"))
+
+        # if path is a symlink, resolve it
+        if os.path.islink(path):
+            path = os.path.join(os.path.dirname(path), os.readlink(path))
+
+        # ensure intermediate directories exist
+        if not os.path.isdir(os.path.dirname(path)):
+            os.makedirs(os.path.dirname(path))
+
+        if os.path.islink(src) or not os.path.isdir(src):
+            if not os.path.exists(path):
+                shutil.copy(src, path, follow_symlinks=False)
+        else:  # src is a directory
+            if (
+                os.path.isdir(path)
+                and not os.path.islink(path)
+                and len(os.listdir(path)) <= 0
+            ):
+                # dst is a empty dir.  remove it (it will be recreated)
+                os.rmdir(path)
+            if not os.path.exists(path):
+                shutil.copytree(src, path, symlinks=True)
+
+
+class IgnoreRecursive(GlobAction):
+    """x - ignore path or glob recursively"""
+
+    def apply_one(_self, _path):
+        pass  # nothing to due; only applies to cleanup
+
+
+class Ignore(GlobAction):
+    """X - ignore path or glob"""
+
+    def apply_one(_self, _path):
+        pass  # nothing to due; only applies to cleanup
+
+
+class Remove(GlobAction):
+    """r - remove empty dir"""
+
+    def apply_one(self, path):
+        if os.path.islink(path) or not os.path.isdir(path):
+            os.unlink(path)
+        elif len(os.listdir(path)) <= 0:
+            os.rmdir(path)
+
+
+class RemoveRecursive(GlobAction):
+    """R - recursive delete"""
+
+    def apply_one(self, path):
+        if os.path.islink(path) or not os.path.isdir(path):
+            os.unlink(path)
+        else:
+            shutil.rmtree(path, ignore_errors=True)
+
+
+# TODO t T
+
+
+class ModeAdjust(GlobAction):
+    """z - adjust mode/user/group"""
+
+    def apply_one(self, path):
+        if os.path.lexists(path):
+            self._chmod(path)
+            self._chown(path)
+            # TODO restore SELinux context
+
+
+class ModeAdjustRecursive(GlobAction):
+    """Z - adjust mode/user/group recursively"""
+
+    def apply_one(self, path):
+        if os.path.islink(path) or not os.path.isdir(path):
+            # link or file
+            self._chmod(path)
+            self._chown(path)
+            # TODO restore SELinux context
+        else:  # directory
+            for dirname, dirs, files in os.walk(path):
+                for filename in dirs + files:
+                    node = os.path.join(dirname, filename)
+                    self._chmod(node)
+                    self._chown(node)
+                    # TODO restore SELinux context
+
+
+class AttrsSet(GlobAction):
+    """h - set file attributes"""
+
+    arg_function = staticmethod(_parse_attrs)
+    chattr_args = []
+
+    def apply_one(self, path):
+        _run_process(
+            f"change attributes of {path}",
+            ["chattr"] + self.chattr_args + [self.arg, path],
+        )
+
+
+class AttrsSetRecursive(AttrsSet):
+    """H - set file attributes recursively"""
+
+    chattr_args = ["-R"]
+
+
+class ACLsSet(GlobAction):
+    """a - set POSIX ACLs"""
+
+    arg_function = staticmethod(_parse_acls)
+
+    def apply_one(self, path):
+        _run_process(
+            f"clear ACLs of {path}",
+            ["setfacl", "--remove-all", "--", path],
+        )
+        _run_process(
+            f"set ACLs of {path}",
+            ["setfacl", "--modify", self.arg, "--", path],
+        )
+
+
+class ACLsAppend(GlobAction):
+    """a+ - append POSIX ACLs"""
+
+    arg_function = staticmethod(_parse_acls)
+
+    def apply_one(self, path):
+        _run_process(
+            f"set ACLs of {path}",
+            ["setfacl", "--modify", self.arg, "--", path],
+        )
+
+
+class ACLsSetRecursive(GlobAction):
+    """A - set POSIX ACLs recursively"""
+
+    arg_function = staticmethod(_parse_acls)
+
+    def apply_one(self, path):
+        _run_process(
+            f"clear ACLs (recursively) of {path}",
+            ["setfacl", "--recursive", "--remove-all", "--", path],
+        )
+        _run_process(
+            f"set ACLs (recursively) of {path}",
+            [
+                "setfacl",
+                "--recursive",
+                "--physical",
+                "--modify",
+                self.arg,
+                "--",
+                path,
+            ],
+        )
+
+
+class ACLsAppendRecursive(GlobAction):
+    """A+ - append POSIX ACLs recursively"""
+
+    arg_function = staticmethod(_parse_acls)
+
+    def apply_one(self, path):
+        _run_process(
+            f"set ACLs (recursively) of {path}",
+            [
+                "setfacl",
+                "--recursive",
+                "--physical",
+                "--modify",
+                self.arg,
+                "--",
+                path,
+            ],
+        )
+
+
+"""
+Map of action type string to class.
+
+This is an OrderedDict so that the insertion order determines the
+order in which actions on the same path shall be performed.  An
+example of why this is needed is that a file must be created
+before you can set attributes, ACLs, etc.
+
+The order is the order in which they appear in tmpfiles.d(5).  The
+order seems reasonable, although I'm not sure whether it is the
+order that systemd-tmpfiles actually uses.
+
+TODO: % specifiers (at least %b and %m are required)
+
+"""
+ACTION_MAP = collections.OrderedDict(
+    [
+        ("f", FileCreate),
+        ("f+", FileCreateOrTruncate),
+        ("F", FileCreateOrTruncate),  # deprecated alias
+        ("w", FileWrite),
+        ("w+", FileAppend),
+        ("d", DirCreateAndCleanup),
+        ("D", DirCreateAndRemove),
+        ("e", DirCleanup),
+        ("v", SubvolumeCreate_v),
+        ("q", SubvolumeCreate_q),
+        ("Q", SubvolumeCreate_Q),
+        ("L", SymlinkCreate),
+        ("L+", SymlinkRecreate),
+        ("c", CreateCharDev),
+        ("C", Copy),
+        ("x", IgnoreRecursive),
+        ("X", Ignore),
+        ("r", Remove),
+        ("R", RemoveRecursive),
+        ("z", ModeAdjust),
+        ("m", ModeAdjust),  # deprecated alias
+        ("Z", ModeAdjustRecursive),
+        ("h", AttrsSet),
+        ("H", AttrsSetRecursive),
+        ("a", ACLsSet),
+        ("a+", ACLsAppend),
+        ("A", ACLsSetRecursive),
+        ("A+", ACLsAppendRecursive),
+    ]
+)
+
+
+if __name__ == "__main__":
+    main(dry_run="--dry-run" in sys.argv)
